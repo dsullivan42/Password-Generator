835d5cfbbca13f582610717dda66308e191a65c0
Daniel Sullivan <ojojoj89@gmail.com>
2023-09-21 18:19:54 -0500

    adding questions for password length and lower case letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,10 @@
 // Assignment Code
-var letters = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var lowerLetters = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","V"]
 var numbers = [1,2,3,4,5,6,7,8,9,0]
 var symbols = ["!","@","#","$","%","^","&","*","(",")","_","-","=","+","[","{","]","}","|", "/", ",",".","<",">","~"]
 var generateBtn = document.querySelector("#generate");
+var availablePassword= []
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,4 +16,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-passwordCriteria = prompt("What criteria would you like your password to match?");
+function askCharacters(){
+  characters = prompt("How many character's would you like your password to contain? (Please choose a number between 8 and 128");
+    console.log(characters);
+  if (generateBtn) {
+    if((characters > 128) || (characters < 8)) {
+      prompt("You must choose a number between 8 and 128");
+      askCharacters();
+    }
+  else{
+    askLower();
+  }
+  }
+}
+function askLower(){
+  lower = prompt("Would you like to include lower case letters in your password? yes or no").toLowerCase();
+  console.log(lower);
+  if((lower !== "yes") && (lower !== "no")){
+    console.log("Please enter yes or no")
+    askLower();
+  }
+  else if (lower === "yes"){
+    availablePassword = lowerLetters.concat();
+    askUpper();
+  }
+  else{
+    askUpper();
+  }
+}
+
+askCharacters();
